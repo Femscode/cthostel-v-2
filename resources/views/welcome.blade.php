97f68063b123e517de57aa0719f14fc94c471e43
Femscode <fasanyafemi@gmail.com>
2022-10-14 14:35:22 +0100

    new_welcome_mail

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,49 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+<style>html,body { padding: 0; margin:0; }</style>
+<div style="font-family:Arial,Helvetica,sans-serif; line-height: 1.5; font-weight: normal; font-size: 15px; color: #2F3044; min-height: 100%; margin:0; padding:0; width:100%; background-color:#edf2f7">
+	<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;margin:0 auto; padding:0; max-width:600px">
+		<tbody>
+			<tr>
+				<td align="center" valign="center" style="text-align:center; padding: 40px">
+					<a href="https://keenthemes.com/" rel="noopener" target="_blank">
+						<img src="https://cthostel.com/myimages/logo_header.png" style="height: 45px" alt="logo">
+							<tr>
+								<td align="left" valign="center">
+									<div style="text-align:left; margin: 0 20px; padding: 40px; background-color:#ffffff; border-radius: 6px">
+										<!--begin:Email content-->
+										<div style="padding-bottom: 30px; font-size: 17px;">
+											<strong>Welcome to CTHostel!</strong>
+										</div>
+										<div style="padding-bottom: 30px">
+                                        Hello {{$name}}, we are so thrilled you decided to join the CTHostel family! Hats off on making an excellent decision!<br><br> As a special thank you, we'll be giving you a free 1GB of data after you post your first ten(10) hostels.<br><br>
+                                        Creating an hostel takes an approximate time of 12secs, so creating ten(10) takes less than 3Min, if that is not exciting, we don't know what is!<br></div>
+										<div style="padding-bottom: 40px; text-align:center;">
+											<a href="https://cthostel.com/admin" rel="noopener" style="text-decoration:none;display:inline-block;text-align:center;padding:0.75575rem 1.3rem;font-size:0.925rem;line-height:1.5;border-radius:0.35rem;color:#ffffff;background-color:#009EF7;border:0px;margin-right:0.75rem!important;font-weight:600!important;outline:none!important;vertical-align:middle" target="_blank">Create First Hostel</a>
+										</div>
+										<div style="padding-bottom: 30px">We've assigned a webpage for just you alone and a swift dashboard to manage all your hostels. For more enquiries, contact <a href="https://wa.me/2349058744473?text=Hi%20,%20my%20name%20is%20(%20Input%20%20your%20name%20)%20,%20from%20CTHostel%20.%20(%20Type%20your%20message%20here%20)%20.">Pelumi</a>, he will attend to all your questions.</div>
+										<div style="border-bottom: 1px solid #eeeeee; margin: 15px 0"></div>
+										<div style="padding-bottom: 50px; word-wrap: break-all;">
+											<p style="margin-bottom: 10px;">My Dashboard:</p>
+											<a href="https://cthostel.com/admin" rel="noopener" target="_blank" style="text-decoration:none;color: #009EF7">https://cthostel.com/admin</a>
+											<p style="margin-bottom: 10px;">My Webpage:</p>
+											<a href="https://cthostel.com/{{$name}}/{{$id}}" rel="noopener" target="_blank" style="text-decoration:none;color: #009EF7">https://cthostel.com/{{$name}}/{{$id}}</a>
+										</div>
+										<!--end:Email content-->
+										<div style="padding-bottom: 10px">Kind regards,
+										<br>The CTHostel Team. 
+										<tr>
+											<td align="center" valign="center" style="font-size: 13px; text-align:center;padding: 20px; color: #6d6e7c;">
+												<p>Abeokuta, Ogun State, Nigeria.</p>
+												<p>Copyright © 
+												<a href="https://cthostel.com/" rel="noopener" target="_blank">CTHostel</a>.</p>
+											</td>
+										</tr></br></div>
+									</div>
+								</td>
+							</tr>
+						</img>
+					</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
