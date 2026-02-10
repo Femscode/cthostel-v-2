f7c08fb83829c085cfade1d6d239bd612c51e70d
Fasanya Pelumi <39712477+Femscode@users.noreply.github.com>
2026-02-10 03:30:30 +0100

    Remove debug realpath route that exposed internals

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,7 @@ require __DIR__ . '/auth.php';
 // Route::get('/', function () {
 //     return view('frontend.home');
 // });
-// the real route
-Route::any('realpath', function() {
-    $path = __DIR__.'/../'; // set the path to the root directory of your Laravel project
-    $realpath = realpath($path);
-    
-    echo $realpath;
-    
-    dd($realpath);
-});
+
 Route::any('search_property', [MarketController::class, 'search_property'])->name('search_property');
 Route::any('upgrade_account', [FrontendCOntroller::class, 'upgrade_account'])->name('upgrade_account');
 
